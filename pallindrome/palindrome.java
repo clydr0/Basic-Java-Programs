49f78c6cb267bd496cef8300a53b4955e40b05d3
Joshua David <62954129+jodave911@users.noreply.github.com>
2022-09-30 11:34:32 +0530

    Add files via upload

---
+++ pallindrome/palindrome.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class palindrom{
+	public static boolean isPalindrome(String str)
+	{
+		String rev = "";
+		boolean ans = false;
+		for (int i = str.length() - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+		}
+		if (str.equals(rev)) {
+			ans = true;
+		}
+		return ans;
+	}
+	public static void main(String[] args)
+	{
+		String str = "malayalam";
+		str = str.toLowerCase();
+		boolean A = isPalindrome(str);
+		System.out.println(A);
+	}
+}
