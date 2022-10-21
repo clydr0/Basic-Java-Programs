942c96136c8ade39c3109da023194aa6e490f1ba
Joshua David <62954129+jodave911@users.noreply.github.com>
2022-10-21 08:03:43 +0530

    Update Freq.java

--- freq/Freq.java
+++ freq/Freq.java
@@ -2,17 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Freq{
-  public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a string");
-    String x = scan.nextLine();
-    
-    System.out.println("Enter the letter for which to find the freq :");
-    String y = scan.nextLine();
-
-    int n = x.length();
-    int m = x.replace(y,"").length();
-    int total= n-m;
-    System.out.println("The no of occurance is : "+total);
-  }
-}
+    public static void main(String[] args){
+      
+        int [] arr = new int [] {5, 2, 8, 7, 1};     
+        int temp = 0;    
+            
+        //Displaying elements of original array    
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+            
+        //Sort the array in descending order    
+        for (int i = 0; i < arr.length; i++) {     
+            for (int j = i+1; j < arr.length; j++) {     
+               if(arr[i] < arr[j]) {    
+                   temp = arr[i];    
+                   arr[i] = arr[j];    
+                   arr[j] = temp;    
+               }     
+            }     
+        }    
+            
+        System.out.println();    
+            
+        //Displaying elements of array after sorting    
+        System.out.println("Elements of array sorted in descending order: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+}    
