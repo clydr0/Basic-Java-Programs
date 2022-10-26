baa4736aa4eca1f39304659c2fc7a587b1243f0c
Joshua David <62954129+jodave911@users.noreply.github.com>
2022-10-26 17:07:55 +0530

    Update Method.java
    its done
    im done

--- method overloading/Method.java
+++ method overloading/Method.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
 
-class Method1{
-	static void large(int x, int y){
+class Method{
+	void large(int x, int y){
 		if (x>=y)
 			System.out.println("The largest value is: "+x);
 		else 
 			System.out.println("The largest value is: "+y);
 	}
-	static void large(int x, int y, int z){
+	void large(int x, int y, int z){
 		if (x>=y && x>=z)
 			System.out.println("The largest value is: "+x);
 		else if(y>=x && y>=z)
@@ -18,28 +18,18 @@ class Method1{
 	}
 }
 
-public class Method{
+class Main{
 public static void main(String[] args){
 		int a, b, c, la, la2;
 		Scanner scan = new Scanner(System.in);
+		Method meth = new Method();
 		System.out.println("Enter the values of three no :");
 	
 		a=scan.nextInt();
 		b=scan.nextInt();
 		c=scan.nextInt();
 		
-		Method1.large(a ,b);
-		Method1.large(a, b, c);
-		
-	//	if(la>la2){
-	//		System.out.println("The largest value is: "+la);
-	//	}
-	//	else{
-	//		System.out.println("The largest value is: "+la2);
-	//	}
-
-
+		meth.large(a ,b);
+		meth.large(a, b, c);
 	}
 }
-
-	
